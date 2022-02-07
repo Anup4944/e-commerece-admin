41f1c1042c50f5b8e5df10e30833cc2c70ae8a07
Anup <anuppoudel60@gmail.comm>
2022-02-07 21:33:58 +1100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+import mongoose from "mongoose";
+import multer from "multer";
+
+const PORT = 5000;
+
+//midddleware
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+
+//database connection
+mongoose
+  .connect(process.env.MONGO_URL, {})
+  .then(console.log("Database connection successful"))
+  .catch((err) => console.log(err));
+
+//404 return
+app.use((req, res, next) => {
+  const error = new Error("Resources not found");
+  error.status = 404;
+
+  next(error);
+});
+
+//handle error
+import { handleError } from "./utils/errorHandler.js";
+app.use((error, req, res, next) => {
+  handleError(error, res);
+});
+
+app.listen(PORT, (error) => {
+  error && console.log(error);
+  console.log(`Backend server is running at http://localhost:${PORT}`);
+});
