f919e5d31e4137c6bc51177ef56fca97d1921080
Anup <anuppoudel60@gmail.comm>
2022-02-11 13:42:30 +1100

    sidebar created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import { Sidebar } from "./component/sidebar/Sidebar";
 import { Topbar } from "./component/topbar/Topbar";
+import "./app.css";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others"> OTHER PAGES</div>
+      </div>
     </div>
   );
 }
