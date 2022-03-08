7f8894ab915c8b665829488e02bbee2309a7b802
Anup <anuppoudel60@gmail.com>
2022-03-08 23:45:46 +1100

    client backend done

--- backend/routers/client.router.js
+++ backend/routers/client.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllClient } from "../models/client/client.model.js";
+import { getAllClient, getClientById } from "../models/client/client.model.js";
 const router = express.Router();
 
 // GET ALL CLIENTS
@@ -12,6 +12,31 @@ router.get("/", async (req, res) => {
       message: "All users",
       allUsers,
     });
+  } catch (error) {
+    res.send({
+      status: "error",
+      message: "Invalid request",
+    });
+  }
+});
+
+// GET CLIENT BY ID
+router.get("/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+
+    const singleUser = await getClientById(_id);
+
+    singleUser._id
+      ? res.send({
+          status: "success",
+          message: "Here is single user",
+          singleUser,
+        })
+      : res.send({
+          status: "error",
+          message: "No user found",
+        });
   } catch (error) {
     res.send({
       status: "error",
