71f712c7ea26137fb080997dbda13b28bc8e84ae
Anup <anuppoudel60@gmail.com>
2022-03-22 14:40:26 +1100

    working on updating individual table row only

--- frontend/src/component/edit-category-form/EditCategory.jsx
+++ frontend/src/component/edit-category-form/EditCategory.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./editCat.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
 import { DeleteOutline } from "@material-ui/icons";
 
 const EditCategory = () => {
+  //   const [edit, setEdit] = useState(false);
+  const [editContactId, setEditContactId] = useState(null);
+  const dispatch = useDispatch();
   const { categories } = useSelector((state) => state.category);
+  //   const [update, setUpdate] = useState(categories);
+  //   console.log(edit);
+  //   console.log(update);
 
   return (
     <>
@@ -22,21 +28,40 @@ const EditCategory = () => {
               <th>Action</th>
             </tr>
           </thead>
-
           <tbody>
             {categories.length &&
               categories.map((item, index) => {
                 return (
                   <>
-                    <tr>
-                      <td> {item.newCategory}</td>
-                      <td> {item.parentCategory}</td>
-                      <td> {item.createdAt}</td>
-                      <td> {item.updatedAt}</td>
-                      <td>
-                        <button className="categoryListButton">Edit</button>
-                        <DeleteOutline />
-                      </td>
+                    <tr key={index}>
+                      {editContactId === item._id ? (
+                        <>
+                          <form>
+                            <input placeholder="Edit category name" />
+                            <button>Save</button>
+                          </form>
+                        </>
+                      ) : (
+                        <>
+                          <td> {item.newCategory}</td>
+                          <td> {item.parentCategory}</td>
+                          <td> {item.createdAt}</td>
+                          <td> {item.updatedAt}</td>
+                          <td>
+                            <button
+                              className="categoryListButton"
+                              //   onClick={() =>
+                              //
+                              //       ? setEdit(true)
+                              //       : setEdit(false)
+                              //   }
+                            >
+                              Edit
+                            </button>
+                            <DeleteOutline />
+                          </td>
+                        </>
+                      )}
                     </tr>
                   </>
                 );
