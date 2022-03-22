b12caf9b3be7ea5ffaee157d1cf852a90145dcf8
Anup <anuppoudel60@gmail.com>
2022-03-22 13:18:17 +1100

    table modified

--- frontend/src/component/edit-category-form/EditCategory.jsx
+++ frontend/src/component/edit-category-form/EditCategory.jsx
@@ -8,50 +8,29 @@ import { DeleteOutline } from "@material-ui/icons";
 const EditCategory = () => {
   const { categories } = useSelector((state) => state.category);
 
-  const columns = [
-    { field: "_id", headerName: "ID", width: 200 },
-    { field: "newCategory", headerName: "Category Name", width: 120 },
-    { field: "parentCategory", headerName: "Parent Category", width: 120 },
-    { field: "createdAt", headerName: "Created", width: 200 },
-    {
-      field: "updateAt",
-      headerName: "Updated",
-      width: 130,
-    },
-
-    {
-      field: "action",
-      headerName: "Action",
-      width: 160,
-      renderCell: (params) => {
-        return (
-          <>
-            <Link to={"/user/" + params.row._id}>
-              <button className="userListButton">Edit</button>
-            </Link>
-            <DeleteOutline
-              className="userListDelete"
-              // onClick={() => {
-              //   handleOnDelete(params.row._id);
-              // }}
-            />
-          </>
-        );
-      },
-    },
-  ];
   return (
     <div className="editCategory">
-      <h1>Edit Category </h1>
-
-      <DataGrid
-        rows={categories}
-        columns={columns}
-        getRowId={(row) => row._id}
-        pageSize={8}
-        checkboxSelection
-        disableSelectionOnClick
-      />
+      <h1 className="editCatTitle">Edit Category</h1>
+      <table className="editCatTable">
+        <tr>
+          <th>Catgeory Name</th>
+          <th>Parent Catgeory Name</th>
+        </tr>
+        <tbody>
+          {categories.length &&
+            categories.map((item, index) => {
+              return (
+                <>
+                  <tr>
+                    <td> {item.newCategory}</td>
+                    <td> {item.parentCategory}</td>
+                  </tr>
+                  <tr></tr>
+                </>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 };
