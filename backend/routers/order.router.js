01f5e12b15236d6dc4d2e5f4261c67a06a1ba845
Anup <anuppoudel60@gmail.com>
2022-04-11 12:31:50 +1000

    total income generated

--- backend/routers/order.router.js
+++ backend/routers/order.router.js
@@ -43,4 +43,38 @@ router.get("/income", async (req, res) => {
   }
 });
 
+// GET MONTHLY INCOME
+
+router.get("/overall", async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const prevMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+  try {
+    const income = await OrderSchema.aggregate([
+      {
+        $group: {
+          _id: "$status",
+          total: { $sum: "$amount" },
+        },
+      },
+    ]);
+
+    income.length
+      ? res.send({
+          status: "success",
+          message: "Here is monthly income",
+          income,
+        })
+      : res.send({
+          status: "error",
+          message: "No income found",
+        });
+  } catch (error) {
+    res.send({
+      status: "error",
+      message: "Unable to get order , please try again later",
+    });
+  }
+});
+
 export default router;
