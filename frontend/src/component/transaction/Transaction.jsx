fed2d3d8fc27c79f513b0a6dc57f2a06bbb4bfe8
Anup <anuppoudel60@gmail.com>
2022-04-26 17:20:48 +1000

    search feature added on transaction page

--- frontend/src/component/transaction/Transaction.jsx
+++ frontend/src/component/transaction/Transaction.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { transactionAction } from "../../component/featuredInfo/orderAction";
 
 import "./transaction.css";
 
 const Transaction = () => {
+  const [serchBy, setSearchBy] = useState("");
   const dispatch = useDispatch();
 
   const { userExp } = useSelector((state) => state.revenue);
@@ -15,6 +16,11 @@ const Transaction = () => {
 
   return (
     <div className="transactionHead">
+      <input
+        className="searchInput"
+        placeholder="Search by email"
+        onChange={(e) => setSearchBy(e.target.value)}
+      />
       <table className="transactionHeadTable">
         <tr>
           <th className="transTh">Cutomers email</th>
@@ -22,17 +28,27 @@ const Transaction = () => {
         </tr>
 
         {userExp.length &&
-          userExp.map((item, i) => {
-            return (
-              <tr key={i}>
-                <td>
-                  <div className="transEmail">{item._id}</div>
-                </td>
+          userExp
+            .filter((item) => {
+              if (serchBy === "") {
+                return item;
+              } else if (
+                item._id.toLowerCase().includes(serchBy.toLowerCase())
+              ) {
+                return item;
+              }
+            })
+            .map((item, i) => {
+              return (
+                <tr key={i}>
+                  <td>
+                    <div className="transEmail">{item._id}</div>
+                  </td>
 
-                <td className="transAmt">${item.total}</td>
-              </tr>
-            );
-          })}
+                  <td className="transAmt">${item.total}</td>
+                </tr>
+              );
+            })}
       </table>
     </div>
   );
